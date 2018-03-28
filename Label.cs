d41a51024701c74013697e30d1c4409df2438cd4
Christopher Blake Powell <cpowel10@hydra10.eecs.utk.edu>
2018-03-28 19:13:54 -0400

    Commented Label.cs

--- Label.cs
+++ Label.cs
@@ -5,14 +5,19 @@ public class Label : ILabel{
 	public readonly string mName;
 	public readonly uint offset;
 
+	// Name property returns Label name
 	public string Name{
 		get{ return mName; }
 	}
 
+	// Offset property returns address to the
+	// instruction the label refers to
 	public uint Offset{
 		get{ return offset; }
 	}
 
+	// Construction for Label instance to
+	// set name and offset
 	public Label(string labelName, uint address){
 		mName = labelName;
 		offset = address;
